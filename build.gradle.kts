958a8fdf99e89ca48c5c3d67a79d7c2259a46d96
davidjiji <thedavidji@gmail.com>
2025-11-03 17:21:11 +1100

    Add dependencies to build.gradle.kts and create application.yml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,9 @@
 plugins {
-    kotlin("jvm") version "2.0.20"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.5"
+    kotlin("jvm") version "1.9.25"
+    kotlin("plugin.spring") version "1.9.25"
+    kotlin("plugin.jpa") version "1.9.25"
 }
 
 group = "davidji"
@@ -11,6 +15,14 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.flywaydb:flyway-core:10.17.2")
+    implementation("org.flywaydb:flyway-database-postgresql:10.17.2")
+    implementation("org.postgresql:postgresql")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
